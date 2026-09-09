11e6674ba6af1783ce1348878ead744a2ea6bb76
Twinkle204 <1945368364@qq.com>
2026-09-09 12:57:12 +0800

    直接两层循环解决公共最长前缀问题

---
+++ longest_common_prefix.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//ai解答
+//标准纵向扫描
+//以第一个字符串为标准，从第一个字母开始往后推，这样就只用遍历一次字符串数组
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+        for (int i = 0; i < strs[0].size(); ++i) {
+            char c = strs[0][i];
+            for (int j = 1; j < strs.size(); ++j) {
+                if (i >= strs[j].size() || strs[j][i] != c) {
+                    return strs[0].substr(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+};
+
+
---
+++ reverse_string.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        
+    }
+};
